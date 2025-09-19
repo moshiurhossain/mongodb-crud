98f799d6347300eab868cdf838a1512b4243af2c
unknown <pantho883@gmail.com>
2025-09-20 01:02:45 +0600

    schema created in authModel.js

---
+++ src/models/authModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+// create Schema
+const authSchema = new mongoose.Schema({
+    userName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    pasword:{
+        type:String,
+        required:true,
+    },
+})
+// create model
