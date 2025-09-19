1fb456ffe5a7064cbcca8526ebc566b5cd689a7e
unknown <pantho883@gmail.com>
2025-09-20 00:42:17 +0600

    db connected to index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const route = require('./src/routes/routes')
+const { default: mongoose } = require('mongoose')
 const app = express()
 const port = 8000
 
- 
+//  using route
 app.use(route)
+// connecting db
+mongoose.connect('mongodb+srv://mern2404:mern2404@cluster0.clrgxwz.mongodb.net/mern2404?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{console.log('db connected')})
+.catch((err)=>{console.log(err)})
 
-app.listen(port,()=>{console.log(`this server is running on port ${port}`)})
+
+
+
+
+app.listen(port,(err)=>{
+    if(err) return console.log(err)
+    console.log(`this server is running on port ${port}`)
+                         })
